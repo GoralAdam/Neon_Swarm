e5c9c5cce7e07cf286090e011da1732b1b19a4cd
ThickGuy03 <adamgoral@onet.eu>
2026-04-07 10:17:18 +0200

    Exp

--- Assets/_Project/Scripts/ArcadePlayerController.cs
+++ Assets/_Project/Scripts/ArcadePlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArcadePlayerController : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class ArcadePlayerController : MonoBehaviour
     public GameObject bulletPrefab;
     public float fireRate = 1f;
     
+    [Header("Experience System")]
+    public int currentExp = 0;
+    public int maxExp = 10;
+    public Slider expBar;
+    
     private float nextFireTime = 0f;
     private Vector2 currentMoveInput=Vector2.zero;
     public void SetMoveInput(Vector2 input)
@@ -17,6 +23,14 @@ public class ArcadePlayerController : MonoBehaviour
         currentMoveInput = input;
     }
 
+    void Start()
+    {
+        if (expBar != null)
+        {
+            expBar.maxValue = maxExp;
+            expBar.value = currentExp;
+        }
+    }
     void Update()
     {
         transform.position += (Vector3)currentMoveInput * moveSpeed * Time.deltaTime;
@@ -30,6 +44,32 @@ public class ArcadePlayerController : MonoBehaviour
         {
             nextFireTime = Time.time + fireRate;
             GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Exp"))
+        {
+            Destroy(other.gameObject);
+            
+            currentExp++;
+
+            if (expBar != null)
+            {
+                expBar.value = currentExp;
+            }
+            if (currentExp >= maxExp)
+            {
+                currentExp = 0;
+                maxExp += 5;
+                
+                expBar.maxValue = maxExp;
+                expBar.value = currentExp;
+                
+                Debug.Log("Level Up!");
+            }
         }
     }
 }
